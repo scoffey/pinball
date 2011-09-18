5484111506ebaab6b6be3c1de9ac69d97fae7a4a
Santiago Coffey <santiago.coffey@gmail.com>
2011-09-17 23:43:54 -0300

    Importing build file, pinball.jar and run scripts

---
+++ build/pinball.bat
@@ -0,0 +1,2 @@
+
+java -Djava.library.path=../lib/lwjgl/native/win32;../lib -jar pinball.jar ../resources/pinball.x3d
---
+++ build/pinball.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+java -Djava.library.path=../lib/lwjgl/native/linux:../lib -jar pinball.jar ../resources/pinball.x3d
+
+
+
+
